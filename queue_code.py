ccdb763da98f828eeac442805459c798cbb55810
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-10 19:35:51 +0800

    enqueue and comments

--- queue_code.py
+++ queue_code.py
@@ -12,3 +12,11 @@ class Queue:
     def __init__(self):
         self._elements = deque()
 
+    # deque() is a double-ended queue in which elements can be both inserted and deleted from either the left or the right end of the queue. 
+    # Dequeue: Remove an element from the front of the queue.     
+    # Enqueue: Add an element to the end of the queue. 
+
+    def enqueue(self, element):
+        self._elements.append(element)
+        
+    
