d51759177b9168a2f4415942a6829940d8280abe
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-10 21:12:33 +0800

    copy paste code queue for stack

--- stack_code.py
+++ stack_code.py
@@ -1,3 +1,21 @@
 # stack_code.py
 
+#just copy queue_code
 from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+    
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+    
+    def dequeue(self):
+        return self._elements.popleft()
