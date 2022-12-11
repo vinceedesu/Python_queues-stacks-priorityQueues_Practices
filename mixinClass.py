c78baa0f948606271a3af9dcdc4a518e924aea29
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-12 01:02:50 +0800

    mixinClass

---
+++ mixinClass.py
@@ -0,0 +1,35 @@
+# stack_code.py
+#source: https://realpython.com/queue-in-python/#learning-about-the-types-of-queues
+#just copy queue_code and stack_code
+
+from queue_code import Queue
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+    
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+    
+    def dequeue(self):
+        return self._elements.popleft()
+
+# stack class
+
+# calling-out Queue class fron queue_code
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+    
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+    
--- priorityQueue_code.py
+++ priorityQueue_code.py
@@ -1,10 +1,16 @@
+#source: https://realpython.com/queue-in-python/#learning-about-the-types-of-queues
+
 from collections import deque
 from heapq import heappop, heappush
+
+# Handling Corner Cases in Your Priority Queue import
+from itertools import count
 
 class PriorityQueue:
     # array for init self
     def __init__(self):
         self._elements =[]
+        self._counter = count()
         
     # heapush - This function adds an element to the heap without altering the current heap.
     def enqueue_with_priority(self, priority, value):
@@ -12,10 +18,13 @@ class PriorityQueue:
         # heappush(self._elements, (priority,value))
 
         # reserve make the priority negative most to least 3 2 1
-        heappush(self._elements,(-priority,value))
+        # heappush(self._elements,(-priority,value))
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
         
     # heappop - This function is used to remove and return the smallest element from the heap
     def dequeue(self):
         # return heappop(self._elements)
-        return heappop(self._elements)[1]
+        # return heappop(self._elements)[1]
+        return heappop(self._elements)[-1]
     
