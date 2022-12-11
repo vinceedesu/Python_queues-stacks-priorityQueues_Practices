048b6ef89361d3675ce4403da0c7a10c57e9f697
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-12 00:37:25 +0800

    added forgotten command under Stack and put it in test

--- stack_code.py
+++ stack_code.py
@@ -1,26 +1,30 @@
 # stack_code.py
-
+#source: https://realpython.com/queue-in-python/#learning-about-the-types-of-queues
 #just copy queue_code
+
+from queue_code import Queue
 from collections import deque
 
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
+# class Queue:
+#     def __init__(self, *elements):
+#         self._elements = deque(elements)
     
-    def __len__(self):
-        return len(self._elements)
+#     def __len__(self):
+#         return len(self._elements)
 
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
+#     def __iter__(self):
+#         while len(self) > 0:
+#             yield self.dequeue()
 
-    def enqueue(self, element):
-        self._elements.append(element)
+#     def enqueue(self, element):
+#         self._elements.append(element)
     
-    def dequeue(self):
-        return self._elements.popleft()
+#     def dequeue(self):
+#         return self._elements.popleft()
 
 # stack class
+
+# calling-out Queue class fron queue_code
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
--- test.py
+++ test.py
@@ -13,3 +13,21 @@ messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
 messages.dequeue()
+
+#stack terminal/python command 
+from heapq import heappush
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+fruits
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+
+person2 < person3
