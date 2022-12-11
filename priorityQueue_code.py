497aa48ee50a67b7d8e1b4cdfe5185ae22e125be
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-11 09:23:22 +0800

    return heappop and terminal manipulation

--- priorityQueue_code.py
+++ priorityQueue_code.py
@@ -6,6 +6,9 @@ class PriorityQueue:
     def __init__(self):
         self._elements =[]
         
+    # heapush - This function adds an element to the heap without altering the current heap.
     def enqueue_with_priority(self, priority, value):
         heappush(self._elements, (priority,value))
-    
+    # heappop - This function is used to remove and return the smallest element from the heap
+    def dequeue(self):
+        return heappop(self._elements)
