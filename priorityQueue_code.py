57a7e1387daf5d0379f23bc3c4602fa52c774f8f
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-11 23:16:02 +0800

    priorityQ

--- priorityQueue_code.py
+++ priorityQueue_code.py
@@ -8,7 +8,13 @@ class PriorityQueue:
         
     # heapush - This function adds an element to the heap without altering the current heap.
     def enqueue_with_priority(self, priority, value):
-        heappush(self._elements, (priority,value))
+        # least to most  1 2 3
+        # heappush(self._elements, (priority,value))
+
+        # reserve make the priority negative most to least 3 2 1
+        heappush(self._elements(-priority,value))
     # heappop - This function is used to remove and return the smallest element from the heap
     def dequeue(self):
-        return heappop(self._elements)
+        # return heappop(self._elements)
+        return heappop(self._elements)[1]
+    
