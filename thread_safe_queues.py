c9cbf73ba7bfd37ba75790cfa3942300bcdf0f33
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-16 22:10:44 +0800

    add function

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -3,8 +3,16 @@ import argparse
 #queue is a library for queues and the likes
 from queue import LifoQueue, PriorityQueue, Queue
 
+#dictionary for queues
 QUEUE_TYPES = {
     "fifo": Queue,
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
+
+
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
